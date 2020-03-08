56f581418e0281c13de2bfb231cc04436373405d
rhr <romain.hotier@gmail.com>
2020-03-08 19:38:56 +0100

    deletefile test start

---
+++ Flask/app/file/all/test/DeleteFile/test_api.py
@@ -0,0 +1,93 @@
+import unittest
+import requests
+
+from server import factory as factory
+import app.file.all.model as file_model
+import app.file.all.test.DeleteFile.api as api
+
+server = factory.Server()
+api = api.DeleteFile()
+file = file_model.FileTest()
+
+
+class DeleteFile(unittest.TestCase):
+
+    def setUp(self):
+        file.clean()
+
+    def test_0_api_ok(self):
+        tc_file = file_model.FileTest().insert()
+        tc_id = tc_file.get_id()
+        """ call api """
+        url = server.main_url + "/" + api.url + "/" + tc_id
+        response = requests.delete(url, verify=False)
+        """ assert """
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('text/plain', response.headers["Content-Type"])
+        self.assertEqual(response.text, tc_file.get_data())
+
+    def test_1_url_not_found(self):
+        tc_file = file_model.FileTest().insert()
+        tc_id = tc_file.get_id()
+        """ call api """
+        url = server.main_url + "/" + api.url + "x/" + tc_id
+        response = requests.get(url, verify=False)
+        response_body = response.json()
+        """ assert """
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.headers["Content-Type"], 'application/json', )
+        self.assertEqual(response_body[api.rep_code_status], 404)
+        self.assertEqual(response_body[api.rep_code_msg], api.rep_code_msg_error_404_url)
+        self.assertEqual(response_body[api.rep_detail], server.detail_url_not_found)
+
+    def test_2_id_without(self):
+        file_model.FileTest().insert()
+        tc_id = ""
+        """ call api """
+        url = server.main_url + "/" + api.url + "/" + tc_id
+        response = requests.get(url, verify=False)
+        response_body = response.json()
+        """ assert """
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.headers["Content-Type"], 'application/json', )
+        self.assertEqual(response_body[api.rep_code_status], 404)
+        self.assertEqual(response_body[api.rep_code_msg], api.rep_code_msg_error_404_url)
+        self.assertEqual(response_body[api.rep_detail], server.detail_url_not_found)
+
+    def test_2_id_string(self):
+        file_model.FileTest().insert()
+        tc_id = "invalid"
+        """ call api """
+        url = server.main_url + "/" + api.url + "/" + tc_id
+        response = requests.get(url, verify=False)
+        response_body = response.json()
+        """ assert """
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.headers["Content-Type"], 'application/json', )
+        self.assertEqual(response_body[api.rep_code_status], 400)
+        self.assertEqual(response_body[api.rep_code_msg], api.rep_code_msg_error_400)
+        detail = api.create_detail(api.param_id, server.detail_must_be_an_object_id, tc_id)
+        self.assertEqual(response_body[api.rep_detail], detail)
+
+    def test_2_id_object_id_invalid(self):
+        file_model.FileTest().insert()
+        tc_id = "aaaaaaaaaaaaaaaaaaaaaaaa"
+        """ call api """
+        url = server.main_url + "/" + api.url + "/" + tc_id
+        response = requests.get(url, verify=False)
+        response_body = response.json()
+        """ assert """
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.headers["Content-Type"], 'application/json', )
+        self.assertEqual(response_body[api.rep_code_status], 400)
+        self.assertEqual(response_body[api.rep_code_msg], api.rep_code_msg_error_400)
+        detail = api.create_detail(api.param_id, server.detail_doesnot_exist, tc_id)
+        self.assertEqual(response_body[api.rep_detail], detail)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.setUp(DeleteFile())
+
+
+if __name__ == '__main__':
+    unittest.main()
