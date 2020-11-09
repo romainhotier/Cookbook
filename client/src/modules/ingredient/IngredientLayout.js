4ef97155ed9414e745352d3d3add04e5c9f9a4af
Amelie MARIETTE <ameliemariette@live.fr>
2020-11-09 12:56:19 +0100

    init ingredientList

---
+++ client/src/modules/ingredient/IngredientLayout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import IngredientPageList from "./containers/IngredientPageList";
+
+import Routes from "./IngredientRoutes";
+
+export const IngredientLayout = () => (
+  <Switch>
+    <Route path={Routes.ingredient()} exact component={IngredientPageList} />
+  </Switch>
+);
---
+++ client/src/modules/ingredient/containers/IngredientPageList/index.js
@@ -0,0 +1,1 @@
+export {default} from './IngredientPageList.container.js'
