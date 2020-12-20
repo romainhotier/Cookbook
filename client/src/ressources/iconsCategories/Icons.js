d850c39b0d9192b3ae00de377f9500b4b219bcbb
Amelie MARIETTE <ameliemariette@live.fr>
2020-12-20 12:38:22 +0100

    add pictos ingredients

---
+++ client/src/ressources/iconsCategories/Icons.js
@@ -0,0 +1,8 @@
+export { default as aperitifs } from './aperitifs.svg'
+export { default as biscuit } from './biscuit.svg'
+export { default as cheatmeal } from './cheatmeal.svg'
+export { default as gateau } from './gateau.svg'
+export { default as healthy } from './healthy.svg'
+export { default as petitdej } from './petitdej.svg'
+export { default as recette_sucree } from './recette-sucree.svg'
+export { default as recette_salee } from './recette-salee.svg'
